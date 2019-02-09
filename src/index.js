395d0cfc13297c025abc8bc15aec82f25f352b82
Paige Williams <paw145@humboldt.edu>
2019-02-08 16:03:54 -0800

    Add interval for clock.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-const greeting = React.createElement('h1', {}, 'Hello, world!!');
-const clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-const app = React.createElement('div', {}, greeting, clock);
+function liveTime(){
+  const greeting = React.createElement('h1', {}, 'Hello, world!!');
+  const clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  const app = React.createElement('div', {}, greeting, clock);
 
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval(liveTime, 1000);
+
