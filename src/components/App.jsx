b1245a48ec4e33508cf8f01bfdd0ec20f6ec2925
Paige Williams <paw145@humboldt.edu>
2019-02-10 16:38:31 -0800

    Create Ticket, Header, and TicketList component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList"
 
 function App(){
   return (
     <div>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>Paige and Harrison</h3>
-      <p><em>Firebase entries are not saving!</em></p>
-      <hr/>
+       <Header/>
+       <TicketList/>
     </div>
   );
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket(){
+  return(
+    <div>
+      <h3>3a</h3>
+      <h3>Paige and Harrison</h3>
+      <p><em>Firebase entries are not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
