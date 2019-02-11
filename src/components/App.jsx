fa54692f5fda62ab73fe3baeb68740c7c5305234
Paige Williams <paw145@humboldt.edu>
2019-02-10 16:27:37 -0800

    Move HTML to App component.

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Paige and Harrison</h3>
+      <p><em>Firebase entries are not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
