edd67e05d36852fb7d93575b1cac3d391a53f860
Paige Williams <paw145@humboldt.edu>
2019-02-16 15:49:54 -0800

    Create form component with state.

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class NewTicketControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+  render(){
+    return (
+      <div>
+        <p>Working!</p>
+      </div>
+    );
+  }
+}
+export default NewTicketControl;
