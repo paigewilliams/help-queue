04013a1d69d7beae3c73817b934546edd8ba0ecd
Paige Williams <paw145@humboldt.edu>
2019-02-17 14:31:37 -0800

    Lift state and render user input in ticket list.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
 
 class NewTicketControl extends React.Component {
   constructor(props){
@@ -17,12 +18,10 @@ class NewTicketControl extends React.Component {
   
   render(){
     let currentlyVisibleContent = null;
-    
     if(this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewTicketForm />;
+      currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation} />;
     } else {
       currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
-      console.log(currentlyVisibleContent);
     }
     
     return(
@@ -31,5 +30,9 @@ class NewTicketControl extends React.Component {
       </div>
     );
   }
+}
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
 }
 export default NewTicketControl;
