ebac05a21720a24347533fb086561341aeb5b0f9
Paige Williams <paw145@humboldt.edu>
2019-02-25 18:58:32 -0800

    Add test to add object to masterArray; passing.

--- _tests_/reducers/ticket-list-reducer.test.js
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -1,5 +1,39 @@
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
-  test('Temporary dummy test', () => {
-    expect(2 + 2).toBe(5);
+
+  let action;
+  const sampleTicketData = {
+    names : 'Paige & Harrison',
+    location : '4b',
+    issue : 'Jest is being a diva and wont work with webpack',
+    timeOpen : 1500000000000,
+    id: 0
+  };
+  
+  test('Should return a default state if no action type is recognized', () => {
+    expect(ticketListReducer( {}, { type: null})).toEqual({});
   });
+
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names, 
+      location: location,
+      issue: issue, 
+      timeOpen: timeOpen,
+      id: id
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue, 
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+  });
+
 });
